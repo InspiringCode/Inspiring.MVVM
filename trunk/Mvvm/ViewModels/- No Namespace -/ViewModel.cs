be4aca3b04e86ace052f0a318b1c20458e8f009f
InspiringCode_cp <SND\InspiringCode_cp>
2010-11-17 15:12:05 +0000

    Minor add.

---
+++ trunk/Mvvm/ViewModels/- No Namespace -/ViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Inspiring.Mvvm.ViewModels {
+   using System.ComponentModel;
+
+   internal sealed class ViewModelNew : CustomTypeDescriptor {
+      public override PropertyDescriptorCollection GetProperties() {
+
+         return base.GetProperties();
+      }
+   }
+}
