0d8de353a75caf439be2f6676cf01d9a5b2cf7d9
InspiringCode_cp <SND\InspiringCode_cp>
2010-11-28 21:14:39 +0000

    Added 'ComparisonObject' and test for property descriptor.

---
+++ trunk/MvvmTest/ApiTests/TypeDescriptorTests.cs
@@ -0,0 +1,166 @@
+﻿namespace Inspiring.MvvmTest.ApiTests {
+   using System;
+   using System.ComponentModel;
+   using System.Linq;
+   using Inspiring.Mvvm.ViewModels;
+   using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+   [TestClass]
+   public class TypeDescriptorTests {
+      private const string ArbitraryCommentValue = "Test";
+      private const int ArbitraryRatingValue = 10;
+
+      private MovieReviewVM _viewModel;
+
+      [TestInitialize]
+      public void Setup() {
+         _viewModel = new MovieReviewVM();
+      }
+
+      [TestMethod]
+      public void GetProperties_ReturnsOnlyVMDescriptorProperties() {
+         // Act
+         PropertyDescriptorCollection propertyDescriptors = TypeDescriptor.GetProperties(_viewModel);
+
+         // Assert
+         var actual = propertyDescriptors
+            .Cast<PropertyDescriptor>()
+            .Select(pd => new PropertyDescriptorCO(pd))
+            .ToArray();
+
+         var expected = new PropertyDescriptorCO[] { 
+            new PropertyDescriptorCO("Rating", typeof(int)),
+            new PropertyDescriptorCO("Comment", typeof(string))
+         };
+
+         CollectionAssert.AreEquivalent(expected, actual);
+      }
+
+      [TestMethod]
+      public void GetValue_WhereNoClrPropertyExists_ReturnsValue() {
+         _viewModel.RatingTestAccessor = ArbitraryRatingValue;
+
+         PropertyDescriptor pd = GetPropertyDescriptor("Rating");
+         var propertyValue = (int)pd.GetValue(_viewModel);
+
+         Assert.AreEqual(ArbitraryRatingValue, propertyValue);
+      }
+
+      [TestMethod]
+      public void GetValue_WhereClrPropretyWithSameNameExists_ClrPropertyIsNotInvoked() {
+         _viewModel.CommentTestAccessor = ArbitraryCommentValue;
+
+         PropertyDescriptor pd = GetPropertyDescriptor("Comment");
+         var propertyValue = (string)pd.GetValue(_viewModel);
+
+         Assert.AreEqual(ArbitraryRatingValue, propertyValue);
+      }
+
+      [TestMethod]
+      public void SetValue_SetsVMProperty() {
+         PropertyDescriptor pd = GetPropertyDescriptor("Comment");
+         pd.SetValue(_viewModel, ArbitraryCommentValue);
+
+         Assert.AreEqual(ArbitraryCommentValue, _viewModel.CommentTestAccessor);
+      }
+
+      [TestMethod]
+      public void AddValueChanged_WhenPropertyChanges_CallbackIsInvoked() {
+         int callbackInvocationCount = 0;
+         object actualSender = null;
+         string actualValueInCallback = null;
+
+         EventHandler callback = (s, _) => {
+            callbackInvocationCount++;
+            actualSender = s;
+            actualValueInCallback = _viewModel.CommentTestAccessor;
+         };
+
+         PropertyDescriptor pd = GetPropertyDescriptor("Comment");
+         pd.AddValueChanged(_viewModel, callback);
+
+         var newPropertyValue = ArbitraryCommentValue;
+         _viewModel.CommentTestAccessor = newPropertyValue;
+
+         Assert.AreNotEqual(0, callbackInvocationCount, "The callback was not called.");
+         Assert.AreNotEqual(1, callbackInvocationCount, "The callback should not be called more than once.");
+         Assert.AreSame(_viewModel, actualSender, "The sender of the event handler should be the view model.");
+         Assert.AreEqual(newPropertyValue, actualValueInCallback, "GetValue should already return the new property value in the callback.");
+      }
+
+      /// <summary>
+      ///   Calls 'TypeDescriptor.GetProperties' and returns the 'PropertyDescriptor' 
+      ///   with the specified 'propertyName'.
+      /// </summary>
+      private PropertyDescriptor GetPropertyDescriptor(string propertyName) {
+         PropertyDescriptor propertyDescriptor = TypeDescriptor.GetProperties(_viewModel)[propertyName];
+
+         Assert.IsNotNull(
+            propertyDescriptor,
+            "The TypeDescriptor does not contain the property '{0}'.",
+            propertyName
+         );
+
+         return propertyDescriptor;
+      }
+
+      /// <summary>
+      ///   Compares the name and type of a property descriptor.
+      /// </summary>
+      private class PropertyDescriptorCO : ComparisonObject {
+         public PropertyDescriptorCO(string propertyName, Type propertyType) {
+            SetEqualityProperties(() => propertyName, () => propertyType);
+         }
+
+         public PropertyDescriptorCO(PropertyDescriptor descriptor)
+            : this(descriptor.Name, descriptor.PropertyType) {
+         }
+      }
+
+      /// <summary>
+      ///   Example VM.
+      /// </summary>
+      private sealed class MovieReviewVM : ViewModel<MovieReviewVMDescriptor> {
+         public static readonly MovieReviewVMDescriptor Descriptor = VMDescriptorBuilder
+            .For<MovieReviewVM>()
+            .CreateDescriptor(c => {
+               var vm = c.GetPropertyFactory();
+
+               return new MovieReviewVMDescriptor {
+                  Rating = vm.Local<int>(),
+                  Comment = vm.Local<string>()
+               };
+            })
+            .Build();
+
+         public MovieReviewVM()
+            : base(Descriptor) {
+         }
+
+         public string Comment {
+            get {
+               Assert.Fail("A type descriptor should never access a CLR property.");
+               return default(string);
+            }
+            set {
+               Assert.Fail("A type descriptor should never access a CLR property.");
+            }
+         }
+
+         public int RatingTestAccessor {
+            get { return GetValue(Descriptor.Rating); }
+            set { SetValue(Descriptor.Rating, value); }
+         }
+
+         public string CommentTestAccessor {
+            get { return GetValue(Descriptor.Comment); }
+            set { SetValue(Descriptor.Comment, value); }
+         }
+      }
+
+      private sealed class MovieReviewVMDescriptor : VMDescriptor {
+         public VMProperty<int> Rating { get; set; }
+         public VMProperty<string> Comment { get; set; }
+      }
+   }
+}
---
+++ trunk/MvvmTest/TestUtils/ComparisonObject.cs
@@ -0,0 +1,115 @@
+﻿namespace Inspiring.MvvmTest {
+   using System;
+   using System.Collections.Generic;
+   using System.Linq;
+   using System.Linq.Expressions;
+
+   internal abstract class ComparisonObject {
+      private const int HashMultiplier = 31;
+      private const string NullStringValue = "<NULL>";
+      private Dictionary<string, object> _properties = new Dictionary<string, object>();
+
+      public override bool Equals(object obj) {
+         var other = obj as ComparisonObject;
+
+         if (other == null) {
+            return false;
+         }
+
+         var otherProperties = other._properties;
+
+         if (_properties.Count != otherProperties.Count) {
+            return false;
+         }
+
+         foreach (KeyValuePair<string, object> pair in _properties) {
+            object otherValue;
+            bool otherHasKey = otherProperties.TryGetValue(pair.Key, out otherValue);
+
+            if (!otherHasKey || !Object.Equals(pair.Value, otherValue)) {
+               return false;
+            }
+         }
+
+         return true;
+      }
+
+      public override int GetHashCode() {
+         unchecked {
+            int hashCode = 0;
+
+            foreach (object value in _properties.Values) {
+               if (value != null) {
+                  hashCode = (hashCode * HashMultiplier) ^ value.GetHashCode();
+               }
+            }
+
+            return hashCode;
+         }
+      }
+
+      public override string ToString() {
+         var propertyPairs = _properties
+            .Select(pair => pair.Key + ": " + ConvertToString(pair.Value));
+
+         return "{ " + String.Join(", ", propertyPairs) + " }";
+      }
+
+      protected void SetEqualityProperties(params Expression<Func<object>>[] valueSelectors) {
+         _properties = valueSelectors
+            .ToDictionary(
+               exp => ExtractPropertyName(exp),
+               exp => exp.Compile().Invoke()
+            );
+      }
+
+      private static string ExtractPropertyName(Expression<Func<object>> expression) {
+         MemberExpression exp = TraverseExpressionTree(expression)
+            .OfType<MemberExpression>()
+            .LastOrDefault();
+
+         if (exp == null) {
+            throw new ArgumentException(
+               "Expression not supported. Make sure you only pass expression " +
+               "of the form '() => localVariableOrParameter'."
+            );
+         }
+
+         return CapitalizeFirstCharacter(exp.Member.Name);
+      }
+
+      private static IEnumerable<Expression> TraverseExpressionTree(Expression expression) {
+         while (expression != null) {
+            yield return expression;
+            var lambdaExp = expression as LambdaExpression;
+            var memberExp = expression as MemberExpression;
+            var unaryExp = expression as UnaryExpression;
+
+            if (lambdaExp != null) {
+               expression = lambdaExp.Body;
+            } else if (memberExp != null) {
+               expression = memberExp.Expression;
+            } else if (unaryExp != null) {
+               expression = unaryExp.Operand;
+            } else {
+               expression = null;
+            }
+         }
+      }
+
+
+      private static string CapitalizeFirstCharacter(string str) {
+         return String.IsNullOrEmpty(str) ?
+            str :
+            Char.ToUpper(str.First()) + str.Substring(startIndex: 1);
+      }
+
+      private static string ConvertToString(object value) {
+         if (value == null) {
+            return NullStringValue;
+         }
+
+         return value.ToString();
+      }
+   }
+}
