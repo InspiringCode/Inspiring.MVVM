a8c97b790bac4dac1df90f076ad409822d5f09ec
BerndKaufmann_cp <SND\BerndKaufmann_cp>
2011-03-22 14:05:14 +0000

    CountingSet class implemented.

---
+++ Dev/Source/Mvvm/ViewModels/Core/CountingSet.cs
@@ -0,0 +1,50 @@
+﻿namespace Inspiring.Mvvm.ViewModels.Core {
+   using System.Collections.Generic;
+
+   /// <summary>
+   ///   Counts the Add operations per item. The item is removed only when the count
+   ///   of the Remove operations matches the count of the Add operations.
+   /// </summary>
+   public class CountingSet<TItem> : IEnumerable<TItem> {
+      private readonly List<TItem> _items = new List<TItem>();
+      private readonly List<KeyValuePair<TItem, int>> _numberedItems = new List<KeyValuePair<TItem, int>>();
+
+      public void Add(TItem item) {
+         int index = _numberedItems.FindIndex(x => x.Key.Equals(item));
+         if (index < 0) {
+            _numberedItems.Add(new KeyValuePair<TItem, int>(item, 1));
+            _items.Add(item);
+            return;
+         }
+         _numberedItems[index] = new KeyValuePair<TItem, int>(
+            _numberedItems[index].Key,
+            _numberedItems[index].Value + 1
+         );
+      }
+
+      public void Remove(TItem item) {
+         int index = _numberedItems.FindIndex(x => x.Key.Equals(item));
+         if (index < 0) {
+            return;
+         }
+
+         if (_numberedItems[index].Value > 1) {
+            _numberedItems[index] = new KeyValuePair<TItem, int>(
+               _numberedItems[index].Key,
+               _numberedItems[index].Value - 1
+            );
+         } else {
+            _numberedItems.RemoveAt(index);
+            _items.Remove(item);
+         }
+      }
+
+      public IEnumerator<TItem> GetEnumerator() {
+         return _items.GetEnumerator();
+      }
+
+      System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
+         return _items.GetEnumerator();
+      }
+   }
+}
---
+++ Dev/Source/MvvmTest/ViewModels/Core/CountingSetTest.cs
@@ -0,0 +1,38 @@
+﻿namespace Inspiring.MvvmTest.ViewModels.Core {
+   using System.Linq;
+   using Inspiring.Mvvm.ViewModels.Core;
+   using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+   [TestClass]
+   public class CountingSetTest {
+
+      [TestMethod]
+      public void RemoveItem_TheSameItemWasAddedTwice_CollectionStillContainsItem() {
+         CountingSet<object> coll = new CountingSet<object>();
+
+         object item = new object();
+
+         coll.Add(item);
+         coll.Add(item);
+
+         coll.Remove(item);
+
+         Assert.IsTrue(coll.Contains(item));
+      }
+
+      [TestMethod]
+      public void RemoveItemTwice_TheSameItemWasAddedTwice_CollectionDoesntContainItem() {
+         CountingSet<object> coll = new CountingSet<object>();
+
+         object item = new object();
+
+         coll.Add(item);
+         coll.Add(item);
+
+         coll.Remove(item);
+         coll.Remove(item);
+
+         Assert.IsFalse(coll.Contains(item));
+      }
+   }
+}
