0da752d3b809319eb754a0bbf533091ee10f7a4a
BerndKaufmann_cp <SND\BerndKaufmann_cp>
2011-04-06 12:39:17 +0000

    Merged latest from main.

---
+++ Dev/Source/Mvvm/Screens/ScreenHierarchyLifecycle.cs
@@ -0,0 +1,13 @@
+﻿namespace Inspiring.Mvvm.Screens {
+   using System.Collections.Generic;
+
+   public class ScreenHierarchyLifecycle : ScreenLifecycle {
+      public ScreenHierarchyLifecycle() {
+         OpenedScreens = new List<IScreenBase>();
+      }
+
+      public IScreenBase Opener { get; set; }
+
+      public ICollection<IScreenBase> OpenedScreens { get; private set; }
+   }
+}
