bf856243f45344b68ed72251bc627b1d1fcba802
BerndKaufmann_cp <SND\BerndKaufmann_cp>
2011-05-04 09:17:23 +0000

    merged from main

---
+++ Dev/Source/Mvvm/Testing/TestScreenResult.cs
@@ -0,0 +1,20 @@
+﻿namespace Inspiring.Mvvm.Testing {
+   using Inspiring.Mvvm.Screens;
+
+   public class TestScreenResult {
+      private readonly DialogLifecycle dialogLifecycle;
+
+      internal TestScreenResult(DialogLifecycle lifecycle) {
+         dialogLifecycle = lifecycle;
+         dialogLifecycle.CloseWindow += (s, e) => {
+            Closed = true;
+         };
+      }
+
+      public bool Closed { get; private set; }
+
+      public DialogScreenResult ScreenResult {
+         get { return dialogLifecycle.ScreenResult; }
+      }
+   }
+}
