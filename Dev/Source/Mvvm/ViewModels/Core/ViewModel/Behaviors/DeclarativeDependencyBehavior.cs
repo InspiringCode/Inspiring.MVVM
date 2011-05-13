ef0934558b38aff8abb6c447609d7813ad941c12
BerndKaufmann_cp <SND\BerndKaufmann_cp>
2011-05-13 10:50:42 +0000

    Bug fixed: HandleChangedNext is called now.

--- Dev/Source/Mvvm/ViewModels/Core/ViewModel/Behaviors/DeclarativeDependencyBehavior.cs
+++ Dev/Source/Mvvm/ViewModels/Core/ViewModel/Behaviors/DeclarativeDependencyBehavior.cs
@@ -12,6 +12,7 @@ namespace Inspiring.Mvvm.ViewModels.Core {
          foreach (var depedency in _dependencies) {
             depedency.HandleChange(context.VM, args);
          }
+         this.HandleChangedNext(context, args);
       }
 
       public void AddDependency(DeclarativeDependency dependency) {
