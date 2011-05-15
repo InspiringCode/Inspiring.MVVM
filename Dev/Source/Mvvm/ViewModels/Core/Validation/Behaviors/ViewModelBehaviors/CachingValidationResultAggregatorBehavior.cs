d35de591985e9fe978e2d9dcf19e8d0747ac6a6d
InspiringCode_cp <SND\InspiringCode_cp>
2011-05-15 16:26:37 +0000

    Cleaned up CachingValidationResultAggregatorBehavior. There is still one problem: Initial population refreshes descendants!

--- Dev/Source/Mvvm/ViewModels/Core/Validation/Behaviors/ViewModelBehaviors/CachingValidationResultAggregatorBehavior.cs
+++ Dev/Source/Mvvm/ViewModels/Core/Validation/Behaviors/ViewModelBehaviors/CachingValidationResultAggregatorBehavior.cs
@@ -1,6 +1,7 @@
 ﻿namespace Inspiring.Mvvm.ViewModels.Core {
    using System;
    using System.Collections.Generic;
+   using System.Diagnostics.Contracts;
    using System.Linq;
 
    internal sealed class CachingValidationResultAggregatorBehavior :
@@ -9,10 +10,10 @@
       IValidationResultAggregatorBehavior,
       IChangeHandlerBehavior {
 
-      private DynamicFieldAccessor<Cache> _cache;
+      private DynamicFieldAccessor<ResultCacheManager> _cache;
 
       public void Initialize(BehaviorInitializationContext context) {
-         _cache = new DynamicFieldAccessor<Cache>(context, ViewModel.GeneralFieldGroup);
+         _cache = new DynamicFieldAccessor<ResultCacheManager>(context, ViewModel.GeneralFieldGroup);
          this.InitializeNext(context);
       }
 
@@ -28,99 +29,58 @@
          this.HandleChangedNext(context, args);
       }
 
-      private Cache GetCache(IBehaviorContext context) {
-         Cache c;
+      private ResultCacheManager GetCache(IBehaviorContext context) {
+         ResultCacheManager c;
          if (!_cache.TryGet(context, out c)) {
-            c = new Cache();
+            c = new ResultCacheManager();
             _cache.Set(context, c);
          }
 
          return c;
       }
 
-      private void UpdateResultOfChild(IBehaviorContext context, IViewModel child) {
-         GetCache(context).UpdateResultOfChild(context, child);
-      }
 
-      private class Cache {
-         private readonly List<ChildResult> _childResults = new List<ChildResult>();
+      private class ResultCacheManager {
+         private readonly ChildResultCollection _childResults = new ChildResultCollection();
 
-         public Cache() {
-            PropertiesResult = ValidationResult.Valid;
-            ViewModelResult = ValidationResult.Valid;
-            DescendantResult = ValidationResult.Valid;
-            SelfResult = ValidationResult.Valid;
-            AllResult = ValidationResult.Valid;
-         }
-
-         public ValidationResult PropertiesResult { get; set; }
-         public ValidationResult ViewModelResult { get; set; }
-         public ValidationResult SelfResult { get; set; }
-         public ValidationResult DescendantResult { get; set; }
-         public ValidationResult AllResult { get; set; }
+         private ValidationResult _propertiesResult = ValidationResult.Valid;
+         private ValidationResult _viewModelResult = ValidationResult.Valid;
+         private ValidationResult _selfResult = ValidationResult.Valid;
+         private ValidationResult _allResult = ValidationResult.Valid;
 
          public ValidationResult GetResult(ValidationResultScope scope) {
             switch (scope) {
                case ValidationResultScope.All:
-                  return AllResult;
+                  return _allResult;
                case ValidationResultScope.Self:
-                  return SelfResult;
+                  return _selfResult;
                case ValidationResultScope.Descendants:
-                  return DescendantResult;
+                  return _childResults.JoinedResult;
                case ValidationResultScope.ViewModelValidationsOnly:
-                  return ViewModelResult;
+                  return _viewModelResult;
                case ValidationResultScope.PropertiesOnly:
-                  return PropertiesResult;
+                  return _propertiesResult;
                default:
                   throw new NotSupportedException();
             }
          }
 
+         // We eagerly refresh the caches of all ancestors. Consider the case:
+         //   (1) A validation result change is notified, thus all caches up the
+         //       hierarchy hold invalid validation states.
+         //   (2) The next behavior in this chain may access the cached validation
+         //       result of an ancestor (this is actually the case with the 
+         //       'CollectionValidationController').
+         // 
+         // Note that we do not handle changes that originated from an descendant
+         // because we have already handled them (the caches of all ancestors were
+         // cleared when the change was handled on that descendant).
          public void HandleChange(IBehaviorContext context, ChangeArgs args) {
-            // We eagerly refresh the caches of all ancestors. Consider the case:
-            //   (1) A validation result change is notified, thus all caches up the
-            //       hierarchy hold invalid validation states.
-            //   (2) The next behavior in this chain may access the cached validation
-            //       result of an ancestor (this is actually the case with the 
-            //       'CollectionValidationController').
-            // 
-            // Note that we do not handle changes that originated from an descendant
-            // because we have already handled them (the caches of all ancestors were
-            // cleared when the change was handled on that descendant).
-
+            bool refreshDescendants = false;
+            bool resultsChanged = false;
             var self = context.VM;
+
             switch (args.ChangeType) {
-               case ChangeType.PropertyChanged:
-                  var ownPropertyChanged = args
-                     .ChangedPath
-                     .SelectsOnlyPropertyOf(self)
-                     .Success;
-
-                  if (ownPropertyChanged) {
-                     args.OldItems.ForEach(x => RemoveCachedChildResult(context, x));
-                     args.NewItems.ForEach(x => UpdateResultOfChild(context, x));
-                  }
-                  break;
-
-                  //// TODO: Is there a cleaner way? Should we introduce a own change type? Would maybe be useful!
-                  //bool viewModelPropertyChanged = args.ChangedProperty != null ?
-                  //   PropertyTypeHelper.IsViewModel(args.ChangedProperty.PropertyType) :
-                  //   false;
-
-                  //if (viewModelPropertyChanged) {
-                  //   var childVM = args
-                  //      .ChangedVM
-                  //      .Kernel
-                  //      .GetValue(args.ChangedProperty) as IViewModel;
-
-                  //   if (childVM != null) {
-                  //      UpdateResultOfChild(context, childVM);
-                  //   } else {
-                  //      // TODO: How can we get the old VM?
-                  //   }
-                  //}
-
-                  break;
                case ChangeType.ValidationResultChanged:
                   var ownViewModelResultChanged = args
                      .ChangedPath
@@ -129,8 +89,7 @@
 
                   if (ownViewModelResultChanged) {
                      RefreshViewModelResult(context);
-                     RefreshDerivedResults();
-                     NotifyBehaviorOfParent(context);
+                     resultsChanged = true;
                   } else {
                      var ownPropertyResultChanged = args
                         .ChangedPath
@@ -139,11 +98,20 @@
 
                      if (ownPropertyResultChanged) {
                         RefreshPropertiesResult(context);
-                        RefreshDerivedResults();
-                        NotifyBehaviorOfParent(context);
+                        resultsChanged = true;
                      }
                   }
                   break;
+
+               case ChangeType.PropertyChanged:
+                  var ownViewModelPropertyMayHaveChanged = args
+                     .ChangedPath
+                     .SelectsOnlyPropertyOf(self)
+                     .Success;
+
+                  refreshDescendants = ownViewModelPropertyMayHaveChanged;
+                  break;
+
                case ChangeType.AddedToCollection:
                case ChangeType.RemovedFromCollection:
                   var ownCollectionContentsChanged = args
@@ -151,11 +119,48 @@
                      .SelectsOnlyCollectionOf(self)
                      .Success;
 
-                  if (ownCollectionContentsChanged) {
-                     args.OldItems.ForEach(x => RemoveCachedChildResult(context, x));
-                     args.NewItems.ForEach(x => UpdateResultOfChild(context, x));
-                  }
+                  refreshDescendants = ownCollectionContentsChanged;
                   break;
+            }
+
+            if (refreshDescendants) {
+               RefreshChildren(
+                  context,
+                  removedChildren: args.OldItems,
+                  newChildren: args.NewItems
+               );
+            } else if (resultsChanged) {
+               RefreshDerivedResults();
+               NotifyBehaviorOfParent(context);
+            }
+         }
+
+         private void RefreshChildren(
+            IBehaviorContext context,
+            IEnumerable<IViewModel> removedChildren = null,
+            IEnumerable<IViewModel> newChildren = null
+         ) {
+            bool descendantsResultChanged = false;
+
+            if (removedChildren != null) {
+               foreach (var c in removedChildren) {
+                  if (_childResults.RemoveCachedResult(c)) {
+                     descendantsResultChanged = true;
+                  }
+               }
+            }
+
+            if (newChildren != null) {
+               foreach (var c in newChildren) {
+                  if (_childResults.UpdateCachedResult(c)) {
+                     descendantsResultChanged = true;
+                  }
+               }
+            }
+
+            if (descendantsResultChanged) {
+               RefreshDerivedResults();
+               NotifyBehaviorOfParent(context);
             }
          }
 
@@ -170,11 +175,11 @@
                .GetValidationResultNext(context)
             );
 
-            PropertiesResult = ValidationResult.Join(results);
+            _propertiesResult = ValidationResult.Join(results);
          }
 
          private void RefreshViewModelResult(IBehaviorContext context) {
-            ViewModelResult = context
+            _viewModelResult = context
                .VM
                .Descriptor
                .Behaviors
@@ -182,8 +187,8 @@
          }
 
          private void RefreshDerivedResults() {
-            SelfResult = ValidationResult.Join(PropertiesResult, ViewModelResult);
-            AllResult = ValidationResult.Join(SelfResult, DescendantResult);
+            _selfResult = ValidationResult.Join(_propertiesResult, _viewModelResult);
+            _allResult = ValidationResult.Join(_selfResult, _childResults.JoinedResult);
          }
 
          private void NotifyBehaviorOfParent(IBehaviorContext context) {
@@ -195,58 +200,85 @@
                   .TryGetBehavior<CachingValidationResultAggregatorBehavior>(out parentBehavior);
 
                if (parentHasBehavior) {
-                  parentBehavior.UpdateResultOfChild(parent.GetContext(), context.VM);
+                  var parentContext = parent.GetContext();
+                  var parentCacheManager = parentBehavior.GetCache(parentContext);
+
+                  parentCacheManager.RefreshChildren(
+                     parentContext,
+                     newChildren: new[] { context.VM }
+                  );
                }
             }
          }
+      }
 
-         public void UpdateResultOfChild(IBehaviorContext context, IViewModel child) {
+      /// <summary>
+      ///   A cache that holds all invalid direct children of the corrent VM and
+      ///   manages them performantly.
+      /// </summary>
+      private class ChildResultCollection {
+         private readonly List<ChildResultEntry> _entries = new List<ChildResultEntry>();
+
+         public ChildResultCollection() {
+            JoinedResult = ValidationResult.Valid;
+         }
+
+         public ValidationResult JoinedResult {
+            get;
+            private set;
+         }
+
+         public bool RemoveCachedResult(IViewModel child) {
+            Contract.Requires(child != null);
+
+            int removals = _entries.RemoveAll(x => x.Child == child);
+            bool joinedResultHasChanged = removals >= 1;
+
+            if (joinedResultHasChanged) {
+               RefreshJoinedResult();
+            }
+
+            return joinedResultHasChanged;
+         }
+
+         public bool UpdateCachedResult(IViewModel child) {
+            Contract.Requires(child != null);
+
             var newChildResult = child
                .Kernel
                .GetValidationResult(ValidationResultScope.All);
 
-            bool descendantsResultHasChanged = false;
+            bool joinedResultHasChanged = false;
 
             if (newChildResult.IsValid) {
-               RemoveCachedChildResult(context, child);
+               joinedResultHasChanged = RemoveCachedResult(child);
             } else {
-               var childCache = _childResults.Find(x => x.Child == child);
-               if (childCache == null) {
-                  childCache = new ChildResult(child);
-                  _childResults.Add(childCache);
+               var childEntry = _entries.Find(x => x.Child == child);
+
+               if (childEntry == null) {
+                  childEntry = new ChildResultEntry(child);
+                  _entries.Add(childEntry);
                }
 
-               var oldChildResult = childCache.Result;
-               childCache.Result = newChildResult;
-
-               descendantsResultHasChanged = !newChildResult.Equals(oldChildResult);
-
-               if (descendantsResultHasChanged) {
-                  UpdateDescendantsResult(context);
-               }
+               joinedResultHasChanged = !newChildResult.Equals(childEntry.Result);
+               childEntry.Result = newChildResult;
             }
+
+            if (joinedResultHasChanged) {
+               RefreshJoinedResult();
+            }
+
+            return joinedResultHasChanged;
          }
 
-         private void RemoveCachedChildResult(IBehaviorContext context, IViewModel child) {
-            bool childWasInvalidBefore = _childResults
-               .RemoveAll(x => x.Child == child) > 0;
-
-            if (childWasInvalidBefore) {
-               UpdateDescendantsResult(context);
-            }
-         }
-
-         private void UpdateDescendantsResult(IBehaviorContext context) {
-            DescendantResult = ValidationResult.Join(
-               _childResults.Select(x => x.Result)
+         private void RefreshJoinedResult() {
+            JoinedResult = ValidationResult.Join(
+               _entries.Select(x => x.Result)
             );
-
-            RefreshDerivedResults();
-            NotifyBehaviorOfParent(context);
          }
 
-         private class ChildResult {
-            public ChildResult(IViewModel child) {
+         private class ChildResultEntry {
+            public ChildResultEntry(IViewModel child) {
                Child = child;
                Result = ValidationResult.Valid;
             }
