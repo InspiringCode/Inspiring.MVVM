aaa5e05bb79c4f2d70d46a753aca553d1869769a
BerndKaufmann_cp <SND\BerndKaufmann_cp>
2011-05-17 08:58:17 +0000

    SelectedItem/Items is public again.

--- Dev/Source/MvvmContrib/ViewModels/MultiSelection/MultiSelectionVMDescriptor.cs
+++ Dev/Source/MvvmContrib/ViewModels/MultiSelection/MultiSelectionVMDescriptor.cs
@@ -1,15 +1,14 @@
 ﻿namespace Inspiring.Mvvm.ViewModels {
    using System.Collections.Generic;
-   using Inspiring.Mvvm.ViewModels.Core;
 
    public class MultiSelectionVMDescriptor<TItemSource, TItemVM> :
       VMDescriptor
       where TItemVM : IViewModel {
 
-      public IVMPropertyDescriptor<IEnumerable<TItemSource>> AllSourceItems { get; set; }
+      internal IVMPropertyDescriptor<IEnumerable<TItemSource>> AllSourceItems { get; set; }
       internal IVMPropertyDescriptor<ICollection<TItemSource>> SelectedSourceItems { get; set; }
       public IVMPropertyDescriptor<IVMCollection<TItemVM>> AllItems { get; set; }
-      internal IVMPropertyDescriptor<IVMCollection<TItemVM>> SelectedItems { get; set; }
+      public IVMPropertyDescriptor<IVMCollection<TItemVM>> SelectedItems { get; set; }
    }
 
    public class MultiSelectionVMDescriptor<TItemSource> :
--- Dev/Source/MvvmContrib/ViewModels/SingleSelection/SingleSelectionVMDescriptor.cs
+++ Dev/Source/MvvmContrib/ViewModels/SingleSelection/SingleSelectionVMDescriptor.cs
@@ -1,15 +1,14 @@
 ﻿namespace Inspiring.Mvvm.ViewModels {
    using System.Collections.Generic;
-   using Inspiring.Mvvm.ViewModels.Core;
 
    public class SingleSelectionVMDescriptor<TItemSource, TItemVM> :
       VMDescriptor
       where TItemVM : IViewModel {
 
-      public IVMPropertyDescriptor<IEnumerable<TItemSource>> AllSourceItems { get; set; }
+      internal IVMPropertyDescriptor<IEnumerable<TItemSource>> AllSourceItems { get; set; }
       internal IVMPropertyDescriptor<TItemSource> SelectedSourceItem { get; set; }
       public IVMPropertyDescriptor<IVMCollection<TItemVM>> AllItems { get; set; }
-      internal IVMPropertyDescriptor<TItemVM> SelectedItem { get; set; }
+      public IVMPropertyDescriptor<TItemVM> SelectedItem { get; set; }
    }
 
    public class SingleSelectionVMDescriptor<TItemSource> :
