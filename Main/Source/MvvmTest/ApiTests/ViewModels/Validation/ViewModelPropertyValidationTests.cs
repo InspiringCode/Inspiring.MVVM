3eef4fda12491469e1823b905edd5a4e6f5a9c8b
InspiringCode_cp <SND\InspiringCode_cp>
2011-11-16 14:49:51 +0000

    Fixed stale validation result cache bug.

--- Main/Source/MvvmTest/ApiTests/ViewModels/Validation/ViewModelPropertyValidationTests.cs
+++ Main/Source/MvvmTest/ApiTests/ViewModels/Validation/ViewModelPropertyValidationTests.cs
@@ -1,12 +1,12 @@
 ﻿namespace Inspiring.MvvmTest.ApiTests.ViewModels.Validation {
    using System;
    using System.Linq;
-   using Microsoft.VisualStudio.TestTools.UnitTesting;
    using Inspiring.Mvvm.ViewModels;
    using Inspiring.Mvvm.ViewModels.Core;
+   using Microsoft.VisualStudio.TestTools.UnitTesting;
 
    [TestClass]
-   public class ViewModelPropertyValidationTests: ValidationTestBase {
+   public class ViewModelPropertyValidationTests : ValidationTestBase {
       [TestMethod]
       public void SetValue_ToNewChild_RevalidatesOldAndNewChild() {
          var vm = new ParentVM();
@@ -22,14 +22,95 @@
          ValidationAssert.ErrorMessages(oldChild.ValidationResult, oldChild.ChildPropertyErrorMessage);
          ValidationAssert.ErrorMessages(newChild.ValidationResult, newChild.ChildPropertyErrorMessage);
       }
-      
+
+      [TestMethod] // regression test
+      public void SetValue_WhenOldAndNewChildGetInvalid_ParentResultContainsOnlyErrorOfNewChild() {
+         var parent = new ParentVM();
+
+         var oldChild = new ChildVM();
+         var newChild = new ChildVM();
+
+         parent.SetValue(x => x.Child, oldChild);
+
+         oldChild.ChildPropertyErrorMessage = "Old validation error";
+         newChild.ChildPropertyErrorMessage = "New validation error";
+         parent.SetValue(x => x.Child, newChild);
+
+         ValidationAssert.ErrorMessages(
+            parent.ValidationResult,
+            ParentVM.ChildInvalidMessage,
+            newChild.ChildPropertyErrorMessage
+         );
+      }
+
+      [TestMethod] // regression test
+      public void SetValue_WhenOldChildGetsInvalidAndNewChildIsValid_ParentValidationResultIsValid() {
+         var parent = new ParentVM();
+
+         var oldChild = new ChildVM();
+         var newChild = new ChildVM();
+
+         parent.SetValue(x => x.Child, oldChild);
+
+         oldChild.ChildPropertyErrorMessage = "Old validation error";
+         parent.SetValue(x => x.Child, newChild);
+
+         ValidationAssert.IsValid(parent.ValidationResult);
+      }
+
+      [TestMethod] // regression test
+      public void SetValue_WhenOldChildGetsSameErrorAsNewChild_ParentValidationResultContainsErrorOfNewChild() {
+         var parent = new ParentVM();
+
+         var oldChild = new ChildVM();
+         var newChild = new ChildVM();
+
+         parent.SetValue(x => x.Child, oldChild);
+
+         string sameValidationError = "Same validation error";
+         oldChild.ChildPropertyErrorMessage = sameValidationError;
+         newChild.ChildPropertyErrorMessage = sameValidationError;
+
+         parent.SetValue(x => x.Child, newChild);
+
+         ValidationError oldError = oldChild
+            .ValidationResult
+            .Errors
+            .Single();
+
+         ValidationError newError = newChild
+            .ValidationResult
+            .Errors
+            .Single();
+
+         Assert.AreNotSame(oldError, newError);
+
+         bool newErrorIsContained = parent
+            .ValidationResult
+            .Errors
+            .Any(x => Object.ReferenceEquals(x, newError));
+
+         bool oldErrorIsContained = parent
+            .ValidationResult
+            .Errors
+            .Any(x => Object.ReferenceEquals(x, oldError));
+
+         Assert.IsFalse(oldErrorIsContained);
+         Assert.IsTrue(newErrorIsContained);
+      }
+
       private sealed class ParentVM : ViewModel<ParentVMDescriptor> {
+         public const string ChildInvalidMessage = "Child is invalid";
+
          public static readonly ParentVMDescriptor ClassDescriptor = VMDescriptorBuilder
             .OfType<ParentVMDescriptor>()
             .For<ParentVM>()
             .WithProperties((d, b) => {
                var v = b.GetPropertyBuilder();
                d.Child = v.VM.Of<ChildVM>();
+            })
+            .WithValidators(b => {
+               b.PropagateChildErrors(ChildInvalidMessage);
             })
             .Build();
 
@@ -51,6 +132,7 @@
                d.ChildProperty = v.Property.Of<string>();
             })
             .WithValidators(b => {
+               b.PropagateChildErrors("child is invalid");
                b.Check(x => x.ChildProperty).Custom(args => {
                   var message = args.Owner.ChildPropertyErrorMessage;
                   if (message != null) {
