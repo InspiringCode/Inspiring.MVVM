deedd50e3dd210262b9406c55b5a97b7f6840da2
cremor_cp <SND\cremor_cp>
2012-01-24 08:04:34 +0000

    Selection fix (use only ReferenceEquality).

--- Main/Source/Mvvm/Common/ReferenceEqualityComparer.cs
+++ Main/Source/Mvvm/Common/ReferenceEqualityComparer.cs
@@ -4,6 +4,17 @@
    using System.Runtime.CompilerServices;
 
    internal sealed class ReferenceEqualityComparer<T> : EqualityComparer<T> {
+      private ReferenceEqualityComparer() {
+      }
+
+      public static EqualityComparer<T> CreateSmartComparer() {
+         if (typeof(T).IsValueType) {
+            return EqualityComparer<T>.Default;
+         } else {
+            return new ReferenceEqualityComparer<T>();
+         }
+      }
+
       public override bool Equals(T x, T y) {
          return Object.ReferenceEquals(x, y);
       }
