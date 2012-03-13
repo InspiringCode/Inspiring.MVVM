9f98453398fb2d63e077288f681bbcc8795744b4
InspiringCode_cp <SND\InspiringCode_cp>
2012-03-13 16:13:49 +0000

    Added assembly info.

--- Main/Source/MvvmContrib/Properties/AssemblyInfo.cs
+++ Main/Source/MvvmContrib/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Windows.Markup;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MvvmContrib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("InspiringMvvmContrib")]
+[assembly: AssemblyDescription("inspiringmvvm.codeplex.com")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("MvvmContrib")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCompany("Daniel Berghold")]
+[assembly: AssemblyProduct("InspiringMvvmContrib")]
+[assembly: AssemblyCopyright("Copyright © Daniel Berghold")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
